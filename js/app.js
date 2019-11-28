5fd615fa1370f262ab0ed6301a9ca714d7e1d7f2
Yash Parekh <parekhyashj@gmail.com>
2019-11-27 23:43:47 -0500

    everything working fine after generating score

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-const correctAnswers = ["B","A","A"];
+const correctAnswers = ["B","A","A","A"];
 const form = document.querySelector(".quiz-form");
+const resultSpace = document.querySelector(".result");
+// const percentage = document.querySelector(".percentage");
+let scorePercentage;
 let score = 0;
+let totalScore = 4;
 let userAnswers;
 
 
@@ -11,16 +15,23 @@ form.addEventListener('submit', e => {
 });
 
 function fetchAnswers(){
-  userAnswers = [form.q1.value,form.q2.value,form.q3.value];
+  userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value];
   // console.log(userAnswers);
 }
 
 function checkAnswers(){
+  score=0;
   userAnswers.forEach((element,index) => {
     if(element===correctAnswers[index]){
       score+=1;
     }
+   
   });
-  console.log(score);
+  scorePercentage = (score/totalScore)*100;
+  console.log(scorePercentage);
+  // once the score has been generated display it
+  resultSpace.classList.remove("d-none");
+  document.querySelector("body > div.result.py-4.bg-light.text-center > div > p > span").innerText=`${scorePercentage}%`;
+  console.log(percentage);
 }
 
